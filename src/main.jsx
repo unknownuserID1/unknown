9b2ccfcad4d892a591026dae9ef9d6e91b4f9522
Breitner Ortiz <brayanp13579b@gmail.com>
2026-04-27 17:58:58 -0500

    Cambios en rutas y deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 import App from './App.jsx'
-import MM1 from './pages/MM1.jsx' // 👈 asegúrate de crear este archivo
+import MM1 from './pages/MM1.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/mm1" element={<MM1 />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 )
